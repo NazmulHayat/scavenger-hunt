93e4eedd612e37692d251adf37429884545c74a3
NazmulHayat <nazmulhayat588@gmail.com>
2021-07-10 21:31:04 +0600

    last_init

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,15 @@
 <template>
-    <v-app>
-      <div id="wrapper">
-        <div class="home-content">
-          <Navbar/>
-          <v-main class="bg">
-            <router-view/>
-          </v-main>
-        </div>
-        <div class="background">
-          <div id="particles-js" class="particles-js"></div>
-        </div>
-      </div>
-        <Footer class="footer"/>
-    </v-app>
+   <v-app id = "app">
+     <div> Ki obostha </div>
+   </v-app>
 </template>
 
+
 <script>
-var ParticleJsIsOn = 1;
 
-import "particles.js";
-const config = require("./particles.json");
-import Navbar from '@/components/Navbar.vue';
-
-import Footer from '@/components/Footer.vue';
-
-export default {
-  name: 'App',
-  components: { Navbar, Footer },
-  data: () => ({
-    
-  }),
-  mounted(){
-    document.getElementById("particles-js").style.height=window.screen.height + "px";
-    document.getElementById("particles-js").style.width=window.screen.width + "px";
-    if(ParticleJsIsOn)
-      window.particlesJS('particles-js', config);
-  }
-}
 </script>
 
-<style>
-  
-  #wrapper{
-    position: relative;
-    height: 100%;
-  }
 
-  .home-content{
-      position: relative;
-      z-index: 100;
-      padding-bottom:60px;
-  }
-  .background{
-      top:0;
-      z-index: 0;
-      position: fixed;
-  }
+<style>
+
 </style>
