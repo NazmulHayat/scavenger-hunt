8a2c25d7402803aff0ad78e3f71f8640c783f0ae
FieryRMS <raiyanmohdshahrms@gmail.com>
2021-07-16 15:57:53 +0600

    asd

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -298,64 +298,7 @@ export default {
   
 }
 
-.form__group {
-  position: relative;
-  padding: 15px 0 0;
-  margin-top: 10px;
-}
 
-.form__field {
-  font-family: inherit;
-  width: 100%;
-  border: 0;
-  border-bottom: 2px solid #000000;
-  outline: 0;
-  font-size: 1.3rem;
-  color: rgb(0, 0, 0);
-  padding: 7px 0;
-  background: transparent;
-  transition: border-color 0.2s;
-}
-.form__field::placeholder {
-  color: transparent;
-}
-.form__field:placeholder-shown ~ .form__label {
-  font-size: 1.3rem;
-  cursor: text;
-  top: 20px;
-}
-
-.form__label {
-  position: absolute;
-  top: 0;
-  display: block;
-  transition: 0.2s;
-  font-size: 1rem;
-  font-weight: 900;
-  color: #000000;
-}
-
-.form__field:focus {
-  padding-bottom: 6px;
-  font-weight: 700;
-  border-width: 3px;
-  border-image: linear-gradient(to right, #240606, hsl(5, 34%, 46%));
-  border-image-slice: 1;
-}
-.form__field:focus ~ .form__label {
-  position: absolute;
-  top: 0;
-  display: block;
-  transition: 0.2s;
-  font-size: 1rem;
-  color: #240606;
-  font-weight: 700;
-}
-
-/* reset input */
-.form__field:required, .form__field:invalid {
-  box-shadow: none;
-}
 
 .pp{
   height: 100%;
